6daa02c93fbf3d2c32421259df04aebbe39c4888
jinke zhao <jz3581@columbia.edu>
2022-11-26 21:38:30 +0000

    q6 question

--- models/project2/q6.sql
+++ models/project2/q6.sql
@@ -1,3 +1,17 @@
 {{ config(materialized='table') }}
 
--- write your query here
+with A as(
+    select dst as dst, src as src 
+    from dbt_jzhao.q3 
+),
+B as(
+    select dst as dst, src as src 
+    from dbt_jzhao.q3 
+), 
+C as(
+    select dst as dst, src as src 
+    from dbt_jzhao.q3 
+)
+select count(*) as no_of_triangles
+from A,B,C
+where A.dst = B.src and B.dst = C.src and C.dst = A.src

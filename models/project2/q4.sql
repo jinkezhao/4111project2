91da5fde39855e01e419dfc0c5176d07f0e2d1b3
jinke zhao <jz3581@columbia.edu>
2022-11-26 20:56:52 +0000

    1

--- models/project2/q4.sql
+++ models/project2/q4.sql
@@ -1,3 +1,6 @@
 {{ config(materialized='table') }}
 
--- write your query here
+select g.src,count(*) from dbt_jzhao.q3 as g group by g.src order by count(*) DESC limit 5
+select g.dst,count(*) from dbt_jzhao.q3 as g group by g.dst order by count(*) DESC limit  5
+
+

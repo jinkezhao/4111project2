b82db2493821c67db599cdd73e7ec3d31fb87f22
jinke zhao <jz3581@columbia.edu>
2022-11-27 21:07:52 +0000

    question2

--- models/project2/q2.sql
+++ models/project2/q2.sql
@@ -1,7 +1,7 @@
 {{ config(materialized='table') }}
 
 with tmp as(
-    SELECT FORMAT_DATE('%b',parsed) AS month,FORMAT_DATE('%Y',parsed) AS year
+    SELECT FORMAT_DATE('%b',parsed) AS month,FORMAT_DATE('%Y',parsed) AS year 
     From (SELECT PARSE_TIMESTAMP("%a %b %d %T %z %Y", t.create_time) AS parsed, t.text as info from graph.tweets as t)
     where lower(info) like '%maga%'
 )

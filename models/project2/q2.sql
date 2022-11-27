2734deb1efa017d8c7f42990efac1377a9070abb
jinke zhao <jz3581@columbia.edu>
2022-11-27 20:38:41 +0000

    question2

--- models/project2/q2.sql
+++ models/project2/q2.sql
@@ -1,3 +1,9 @@
 {{ config(materialized='table') }}
 
--- write your query here
+SELECT FORMAT_DATE('%b',parsed) AS month,FORMAT_DATE('%Y',parsed) AS year,count(*) as count
+From (SELECT PARSE_TIMESTAMP("%a %b %d %T %z %Y", t.create_time) AS parsed, t.text AS info from graph.tweets as t)
+where lower(info) like '%maga%' 
+group by parsed
+order by count(*) DESC 
+limit 5
+

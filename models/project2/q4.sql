eff8b4e24a779c36499a28646f6a7fedf9cd05bb
jinke zhao <jz3581@columbia.edu>
2022-11-28 15:26:15 +0000

    question5

--- models/project2/q4.sql
+++ models/project2/q4.sql
@@ -1,20 +1,4 @@
 {{ config(materialized='table') }}
-with outdegree as(
-    select  g.src as src
-    from dbt_jzhao.q3 as g 
-    group by g.src 
-    order by count(*) DESC 
-    limit 1
-),
-indegree as(
-    select  g.dst as dst
-    from dbt_jzhao.q3 as g 
-    group by g.dst 
-    order by count(*) DESC 
-    limit  1
-)
 
-select  indegree.dst as max_indegree, outdegree.src as max_outdegree
-from outdegree, indegree
 
 
